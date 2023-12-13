3674de900cb60d8a9ac682a5552b0cc5c977b9bb
JoseRicoCct <145503102+JoseRicoCct@users.noreply.github.com>
2023-12-13 13:04:14 +0000

    Adding a more similiar dashboard to the one I will be presenting for CA2.

--- app_dash.py
+++ app_dash.py
@@ -1,82 +1,36 @@
-import streamlit as st
-import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import numpy as np
-import yfinance as yf
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 
-st.title('S&P 500 App')
-
-st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-* **Python libraries:** base64, pandas, streamlit, yfinance, numpy, matplotlib
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
-""")
-
-st.sidebar.header('User Input Features')
-
-# Web scraping of S&P 500 data
-#
-@st.cache
-def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    return df
-
-df = load_data()
-sector = df.groupby('GICS Sector')
-
-# Sidebar - Sector selection
-sorted_sector_unique = sorted( df['GICS Sector'].unique() )
-selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
-
-# Filtering data
-df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
-
-st.header('Display Companies in Selected Sector')
-st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
-st.dataframe(df_selected_sector)
-
-# Download S&P500 data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-    return href
-
-st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
-
-# https://pypi.org/project/yfinance/
-
-data = yf.download(
-        tickers = list(df_selected_sector[:10].Symbol),
-        period = "ytd",
-        interval = "1d",
-        group_by = 'ticker',
-        auto_adjust = True,
-        prepost = True,
-        threads = True,
-        proxy = None
+# Load Data
+df = px.data.tips()
+# Build App
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1("Dash Demo"),
+    dcc.Graph(id='graph'),
+    html.Label([
+        "colorscale",
+        dcc.Dropdown(
+            id='colorscale-dropdown', clearable=False,
+            value='plasma', options=[
+                {'label': c, 'value': c}
+                for c in px.colors.named_colorscales()
+            ])
+    ]),
+])
+# Define callback to update graph
+@app.callback(
+    Output('graph', 'figure'),
+    [Input("colorscale-dropdown", "value")]
+)
+def update_figure(colorscale):
+    return px.scatter(
+        df, x="total_bill", y="tip", color="size",
+        color_continuous_scale=colorscale,
+        render_mode="webgl", title="Tips"
     )
-
-# Plot Closing Price of Query Symbol
-def price_plot(symbol):
-  df = pd.DataFrame(data[symbol].Close)
-  df['Date'] = df.index
-  fig = plt.figure()
-  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
-  plt.xticks(rotation=90)
-  plt.title(symbol, fontweight='bold')
-  plt.xlabel('Date', fontweight='bold')
-  plt.ylabel('Closing Price', fontweight='bold')
-  return st.pyplot(fig)
-
-num_company = st.sidebar.slider('Number of Companies', 1, 5)
-
-if st.button('Show Plots'):
-    st.header('Stock Closing Price')
-    for i in list(df_selected_sector.Symbol)[:num_company]:
-        price_plot(i)
+# Run app and display result inline in the notebook
+app.run_server(mode='inline')
