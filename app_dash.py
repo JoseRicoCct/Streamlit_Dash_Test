500679ba5fd90002ebf4bf9b6f0bed4c6f5f6217
JoseRicoCct <145503102+JoseRicoCct@users.noreply.github.com>
2023-12-13 11:07:14 +0000

    Modifiying app_dash.py.

--- app_dash.py
+++ app_dash.py
@@ -1,100 +1,13 @@
-import streamlit as st
+import json
 import pandas as pd
-import numpy as np
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+
 import dash
-import plotly.express as px
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
-import dash_colorscales
-import cufflinks as cf
-import plotly.express as px
-import plotly.graph_objects as go
-import json
-import re
 
-ufo_df = pd.read_csv('scrubbed.csv.zip', low_memory=False)
-
-ufo_df.dropna(inplace=True)
-ufo_df = ufo_df[ufo_df['country'] == 'us']
-
-new_data = pd.read_excel('States.xlsx', engine='openpyxl')
-
-def map_abb(state):
-    for row in new_data.index:
-        if state == new_data['ABB_lower'][row]:
-            return new_data['ABB_upper'][row]
-        
-new_data['test'] = new_data['ABB_lower'].apply(map_abb)
-
-ufo_df['state'] = ufo_df['state'].apply(map_abb)
-
-ufo_df['datetime'] = ufo_df['datetime'].apply(lambda x: x[-10:-6])
-
-ufo_df['datetime'] = ufo_df['datetime'].apply(lambda x: int(x))
-
-ufo_df
-
-ufo_df['datetime'].max()
-
-def bin_decade(year):
-    if year >= 1910 and year <= 1920:
-        return "1910-1920"
-    elif year >= 1920 and year <= 1930:
-        return "1920-1930"
-    elif year >= 1930 and year <= 1940:
-        return "1930-1940"
-    elif year >= 1940 and year <= 1950:
-        return "1940-1950"
-    elif year >= 1950 and year <= 1960:
-        return "1950-1960"
-    elif year >= 1960 and year <= 1970:
-        return "1960-1970"
-    elif year >= 1970 and year <= 1980:
-        return "1970-1980"
-    elif year >= 1980 and year <= 1990:
-        return "1980-1990"
-    elif year >= 1990 and year <= 2000:
-        return "1990-2000" 
-    elif year >= 2000 and year <= 2010:
-        return "2000-2010"
-    elif year >= 2010 and year <= 2014:
-        return "2010-2014"
-    
-ufo_df['decade'] = ufo_df['datetime'].apply(bin_decade)
-
-ufo_df = ufo_df[ufo_df['datetime'] >= 1950]
-
-ufo_df['sighting'] = 1
-
-ufo_df.to_csv('clean_df.csv')
-
-ufo_df.groupby(['decade', 'state']).sum()['sighting']
-
-ufo_df.to_csv('clean_df.csv')
-
-ufo_df.info()
-
-test = pd.read_csv('clean_df.csv', index_col=0)
-
-another_test = test.groupby(['decade', 'state']).sum()
-
-another_test.groupby('decade').sum()
-
-test = test[test['state'] == 'TX']
-
-new_test = test.groupby(['decade', 'state']).sum()
-
-new_test['avg_encounter'] = new_test['duration (seconds)'] / new_test['sighting']
-
-new_test.reset_index(inplace=True)
-
-new_test
-
-new_test['decade'].values
-
-fig = px.bar(new_test, x='decade', y='sighting')
-fig.show()
 df = pd.read_csv("clean_df.csv", index_col=0)
 
 colors = {
