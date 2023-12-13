7aeb1f2dc5b0cbecbd8b4bef665e0f457587d4ec
JoseRicoCct <145503102+JoseRicoCct@users.noreply.github.com>
2023-12-13 13:49:49 +0000

    Using streamlit package to make the app work.

--- app_dash.py
+++ app_dash.py
@@ -1,36 +1,21 @@
+import streamlit as st
 import plotly.express as px
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
 
 # Load Data
 df = px.data.tips()
-# Build App
-app = dash.Dash(__name__)
-app.layout = html.Div([
-    html.H1("Dash Demo"),
-    dcc.Graph(id='graph'),
-    html.Label([
-        "colorscale",
-        dcc.Dropdown(
-            id='colorscale-dropdown', clearable=False,
-            value='plasma', options=[
-                {'label': c, 'value': c}
-                for c in px.colors.named_colorscales()
-            ])
-    ]),
-])
-# Define callback to update graph
-@app.callback(
-    Output('graph', 'figure'),
-    [Input("colorscale-dropdown", "value")]
+
+# Streamlit App
+st.title("Streamlit Demo")
+
+# Dropdown for selecting colorscale
+colorscale = st.selectbox("Select Colorscale", px.colors.named_colorscales())
+
+# Scatter plot
+fig = px.scatter(
+    df, x="total_bill", y="tip", color="size",
+    color_continuous_scale=colorscale,
+    render_mode="webgl", title="Tips"
 )
-def update_figure(colorscale):
-    return px.scatter(
-        df, x="total_bill", y="tip", color="size",
-        color_continuous_scale=colorscale,
-        render_mode="webgl", title="Tips"
-    )
-# Run app and display result inline in the notebook
-app.run_server(mode='inline')
+
+# Display the plot using Streamlit
+st.plotly_chart(fig)
